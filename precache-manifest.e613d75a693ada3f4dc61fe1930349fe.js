aa38a766080ec8fc2521977517b60b8bcd2ccb3d
Namrata Subhedar <nammsy@gmail.com>
2019-11-26 22:37:03 +0530

    Updates

---
+++ precache-manifest.e613d75a693ada3f4dc61fe1930349fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feec75c0e4f88a1319a575743b2f0553",
+    "url": "/ReactEcommerceSite/index.html"
+  },
+  {
+    "revision": "e43107fabbabc185bc26",
+    "url": "/ReactEcommerceSite/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9fa00aa842529c59f360",
+    "url": "/ReactEcommerceSite/static/css/main.8a2fe4e7.chunk.css"
+  },
+  {
+    "revision": "e43107fabbabc185bc26",
+    "url": "/ReactEcommerceSite/static/js/2.18f47ed5.chunk.js"
+  },
+  {
+    "revision": "9fa00aa842529c59f360",
+    "url": "/ReactEcommerceSite/static/js/main.b79d72a7.chunk.js"
+  },
+  {
+    "revision": "db5e8d25424f72ec74fa",
+    "url": "/ReactEcommerceSite/static/js/runtime-main.a6a048b3.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/ReactEcommerceSite/static/media/logo.9f7e506d.svg"
+  }
+]);
